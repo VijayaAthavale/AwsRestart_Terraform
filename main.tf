310f7afe6cde36393a68cda8c40e6a73f9c68ce1
vijayaathavale <vijayas.athavale@gmail.com>
2024-02-23 13:08:59 +0100

    main and providers

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Create a VPC to launch our instances into
+resource "aws_vpc" "dev_vpc" {
+ cidr_block = "10.0.0.0/16"
+ enable_dns_hostnames = true
+ enable_dns_support = true
+ tags    = {
+  name   = "deham10"
+ }
+}
+#resource "aws_internet_gateway" "cpstn_IGW"{
+ #   vpc_id                 = aws_vpc.dev_vpc.id
+  #  tags                   = {
+   #     Name               = "cpstn_vpc_igw"
+    #}
+#}
